b5e219dac6e8ff0fa3bf09b79df010797ae5bd3d
Mike Park <mikep@quake.net>
2013-07-20 20:07:30 +0200

    add view of missing signature reminders for the coming week

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,12 @@
 module HomeHelper
+  def signature_week_reminders(therapist)
+    return [] unless therapist
+
+    (0..6).inject([]) do |memo, i|
+      date = Date.current + i
+      reminders = MissingSignatureService.new(therapist, date.wday).reminders
+      memo += [[date, reminders]] if reminders.any?
+      memo
+    end
+  end
 end
