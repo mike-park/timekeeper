a962e27ca6c183df24130e298a3b8ecf597ecd6e
Mike Park <mikep@quake.net>
2013-11-27 10:29:37 +0100

    fix factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :user do
     name 'Test User'
-    email 'example@example.com'
+    sequence(:email) { |n| "example#{n}@example.com" }
     password 'pleaseplease'
     password_confirmation 'pleaseplease'
     # required if the Devise Confirmable module is used
