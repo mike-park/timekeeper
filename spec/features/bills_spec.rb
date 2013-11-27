b2d439011154f1c5732f2a6063d1070514de3154
Mike Park <mikep@quake.net>
2013-11-27 19:00:52 +0100

    feature test: new bill include toggling & saving

--- spec/features/bills_spec.rb
+++ spec/features/bills_spec.rb
@@ -43,6 +43,20 @@ describe 'Bills', js: true do
     "#{date} #{event.event_category_title}"
   end
 
+  def find_client_event(client, event)
+    find("#client-#{client.id}").find('.bill-item', text: event_title(event))
+  end
+
+  def match_event_included(client, event, result)
+    node = find_client_event(client, event)
+    included = node['class'].include?('event-included')
+    expect([event, included]).to eq([event, result])
+  end
+
+  def match_events_in_bill(bill, events)
+    expect(bill.bill_items.map(&:event_id)).to eq(events.map(&:id))
+  end
+
   it 'should show a new bill' do
     render_on_error do
       goto_new_bill
@@ -62,7 +76,7 @@ describe 'Bills', js: true do
   context "client with 2 events" do
     let(:events) {
       [
-          add_event(client: client, event_category: therapy, occurred_on: Date.yesterday),
+          add_event(client: client, event_category: therapy, occurred_on: Date.current - 1.month),
           add_event(client: client, event_category: therapy1),
       ]
     }
@@ -80,9 +94,77 @@ describe 'Bills', js: true do
     it "should have event names" do
       render_on_error do
         goto_new_bill
-        items = find_client_events(client, 2)
-        (0..1).each do |n|
-          items[n].should have_content(event_title(events[n]))
+        events.each do |event|
+          find_client_event(client, event)
+        end
+      end
+    end
+
+    context "included events" do
+      it "should have 1 event included & the other not included" do
+        render_on_error do
+          goto_new_bill
+          match_event_included(client, events[0], false)
+          match_event_included(client, events[1], true)
+        end
+      end
+
+      it "should have all included events" do
+        render_on_error do
+          goto_new_bill
+          click_on('Include All Activities')
+          events.each do |event|
+            match_event_included(client, event, true)
+          end
+        end
+      end
+
+      it "should have no included events" do
+        render_on_error do
+          goto_new_bill
+          click_on('Exclude All Activities')
+          events.each do |event|
+            match_event_included(client, event, false)
+          end
+        end
+      end
+
+      it "should toggle included events" do
+        render_on_error do
+          goto_new_bill
+          find_client_event(client, events[0]).click
+          find_client_event(client, events[1]).click
+          match_event_included(client, events[0], true)
+          match_event_included(client, events[1], false)
+        end
+      end
+    end
+
+    context "save bill" do
+      it "should save with defaults" do
+        render_on_error do
+          goto_new_bill
+          click_on('Save Bill')
+          page.should have_link('Print Bill')
+          expect(Bill.count).to eq(1)
+          bill = Bill.first
+          match_events_in_bill(bill, [events[1]])
+        end
+      end
+
+      it "should save with changes" do
+        render_on_error do
+          goto_new_bill
+          # angular popup doesn't like capybara fill_in
+          #fill_in(:billedOn, with: '01.12.2013')
+          fill_in(:number, with: 'ABC123')
+          find_client_event(client, events[0]).click
+          click_on('Save Bill')
+          page.should have_link('Print Bill')
+
+          expect(Bill.count).to eq(1)
+          bill = Bill.first
+          match_events_in_bill(bill, events)
         end
       end
     end
